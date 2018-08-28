c682e3e38d6a7a4fd1b6594fb6232335e32fc88a
feanar729 <feanar729@gmail.com>
2018-08-28 16:09:31 +0900

    update & fix: object value 버그 수정 및 md파일 내용 update

--- parser.js
+++ parser.js
@@ -85,16 +85,16 @@ const testcase2 = "[1,[null,[1,4,{name: 'c r o n           g ', live: 'seoul', f
 const testcase3 = "{keyName:'name', value:3213, child:[1,3,true,false,null,['test']]}";
 const testcase4 = "{keyName:'name', value:3213}";
 
-const testcase = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a:'a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]";
-
 const errorcase1 = "{key: ObjKey, value: true, other: 23}";
 const errorcase2 = "[test:'test']"
+
 
 const parser = new Parser({
   countObj: new countDataType(),
   dataTypeObj: new checkDataType()
 });
-const result = parser.parsingObj(testcase);
+
+const result = parser.parsingObj(testcase1);
 // const result2 = parser.parsingObj(errorcase1);
 console.log(JSON.stringify(result, null, 2));
 
